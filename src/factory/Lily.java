a5dd6f0ef164fb126d2b19b23b6b19a3732b8cea
Dinara <dinaramedeubek@icloud.com>
2025-11-08 13:11:41 +0500

    dinara

--- src/factory/Lily.java
+++ src/factory/Lily.java
@@ -4,7 +4,7 @@ import utilities.Money;
 
 public class Lily implements FlowerFactory {
     @Override
-    public Flower collecteFlower(String color) {
+    public Flower collectFlower(String color) {
         return new Flower("Lily", color, Money.of(4200));
     }
 }
